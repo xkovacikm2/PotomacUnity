faa9f1edcd679a29914158fde2c3dab32e0f042a
Michal Kovacik <michal.kovacik@siemens-healthineers.com>
2018-12-17 11:49:45 +0100

    Initial - solved bounce problem

---
+++ Assets/Scripts/BounceRocket.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class BounceRocket : MonoBehaviour
+{
+    public float CorrectionThreshold = 0.5f;
+
+    public void DoBounceRocket(Vector3 reflection)
+    {
+        var rigidBody = GetComponent<Rigidbody>();
+        var correctedReflection = CorrectReflection(reflection);
+        rigidBody.velocity = correctedReflection;
+        Debug.Log(reflection);
+        Debug.Log(correctedReflection);
+    }
+
+    private Vector3 CorrectReflection(Vector3 reflection)
+    {
+        var speed = GetComponent<RocketThrust>().speed;
+
+        return new Vector3(CorrectReflectionValue(speed, reflection.x),
+            CorrectReflectionValue(speed, reflection.y), CorrectReflectionValue(speed, reflection.z));
+    }
+
+    private float CorrectReflectionValue(float thresh, float value)
+    {
+        if (value < -CorrectionThreshold)
+        {
+            return -thresh;
+        }
+
+        if (value > CorrectionThreshold)
+        {
+            return thresh;
+        }
+
+        return 0f;
+    }
+}
---
+++ Assets/Scripts/RocketThrust.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class RocketThrust : MonoBehaviour
+{
+    public float speed;
+
+    private void Start()
+    {
+        var rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = this.transform.forward * this.speed;
+        Debug.Log(rigidbody.velocity);
+    }
+}
