b433ddb158ebfb3bedb88596e4e37e186e3254f6
Michal Kovacik <michal.kovacik@siemens-healthineers.com>
2018-12-17 17:29:31 +0100

    Simple Start Stop Logic

---
+++ Assets/Scripts/ClickHandlers/StartStop.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StartStop : MonoBehaviour
+{
+    public Button button;
+    private bool started;
+
+    private void Start()
+    {
+        button.onClick.AddListener(OnClick);
+        started = false;
+    }
+
+    private void OnClick()
+    {
+        if (started)
+        {
+            Stop();
+        }
+        else
+        {
+            Run();
+        }
+
+        started = !started;
+    }
+
+    private void Run()
+    {
+        var text = button.GetComponentInChildren<Text>();
+        text.text = "Stop";
+
+        var rockets = GameObject.FindGameObjectsWithTag("Rocket");
+        foreach (var rocket in rockets)
+        {
+            rocket.GetComponent<RestartRocket>().SetActiveChildren(true);
+            rocket.GetComponent<RocketThrust>().StartThrust();
+        }
+    }
+
+    private void Stop()
+    {
+        var text = button.GetComponentInChildren<Text>();
+        text.text = "Start";
+        
+        var rockets = GameObject.FindGameObjectsWithTag("Rocket");
+        foreach (var rocket in rockets)
+        {
+            rocket.GetComponent<RestartRocket>().ResetPositionRotation();
+            rocket.GetComponent<RocketThrust>().StopThrust();
+        }
+    }
+}
---
+++ Assets/Scripts/Rockets/RocketThrust.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class RocketThrust : MonoBehaviour
+{
+    public float speed;
+
+    private void Update()
+    {
+        var rigidBody = GetComponent<Rigidbody>();
+
+        if (rigidBody.velocity.magnitude > 0)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                Quaternion.LookRotation(rigidBody.velocity.normalized), 0.2f);
+        }
+    }
+
+    public void StartThrust()
+    {
+        var rigidBody = GetComponent<Rigidbody>();
+        rigidBody.velocity = transform.forward * speed;
+    }
+
+    public void StopThrust()
+    {
+        var rigidBody = GetComponent<Rigidbody>();
+        rigidBody.velocity = transform.forward * 0f;
+    }
+}
